cc5b10671df78e7c1cb530da2e425b276c716c83
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-13 19:52:58 +0300

    Add the solution to 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = [];
+    
+    if (num < 0) {
+        a = 'ERROR';
+        return a;
+    }
 
+    for (i = 0; i < num; i++) {
+        result.push(str);
+    }
+
+    return result.join('');
 };
 
 // Do not edit below this line
