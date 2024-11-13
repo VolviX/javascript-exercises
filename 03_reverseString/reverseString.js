7148b70c2e78650f57f1bcd047589b9be536add2
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-13 21:23:02 +0300

    Add the solution of the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const input = str.split('');
+    const output = [];
 
+    for (let i = input.length - 1; i > -1; i--) {
+        output.push(input[i]);
+    }
+
+    return output.join('');
 };
 
 // Do not edit below this line
