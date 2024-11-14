447bbd4a25ab38547cd2b723e1a3e6c8bc4435fd
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-14 18:30:18 +0300

    Add solution to the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(valueOne, valueTwo) {    
+    if (
+        (!Number.isInteger(valueOne) || !Number.isInteger(valueTwo))
+        || (valueOne < 0 || valueTwo < 0) 
+    ) {
+        return 'ERROR';
+    } 
 
+    if (valueOne <= valueTwo) {
+        min = valueOne;
+        max = valueTwo;
+    } else {
+        min = valueTwo;
+        max = valueOne;
+    }
+
+    let total = 0;
+    for (let i = min; i <= max; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
