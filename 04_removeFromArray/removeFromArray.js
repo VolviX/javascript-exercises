a6dfbaa667985ae6aad3bd3f554f2f78991dcd2e
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-14 17:21:06 +0300

    Add the solution of the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(userInput, ...args) {
+    const result = userInput.filter(item => !args.includes(item));
+
+    return result;
 };
 
 // Do not edit below this line
