c2c0a8885aca7c2787a65385ebc45d8c9ce59ef3
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-30 19:51:05 +0300

    Add solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arg) {
+    let list = [];
 
+	for (let i = 0; i < arg.length; i++) {
+		list.push(arg[i].title);
+	}
+	
+    return list;
 };
 
 // Do not edit below this line
