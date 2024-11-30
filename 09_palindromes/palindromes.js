6ffd71a954d92d2ca9a09cec3be3cfd59a4f1747
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-30 17:49:17 +0300

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (arg) {
 
+    let transformed = arg;
+    const remove = ['!', '.', ',', ' '];
+
+    remove.forEach(item => {
+        if (arg.includes(item)) {
+            transformed = transformed.split(item).join('');
+        }
+    });
+  
+    const reversed = transformed.split('').reverse().join('');
+  
+    return transformed.toLowerCase() == reversed.toLowerCase();
 };
 
 // Do not edit below this line
