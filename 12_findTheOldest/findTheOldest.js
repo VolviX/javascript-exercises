1d086cd8ff126b1073e803109d97b19c112e101a
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-30 21:21:23 +0300

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arg) {
+    let oldest = 0;
+	let name;
+    
+    for (let i = 0; i < arg.length; i++) {
+		
+        if (typeof arg[i].yearOfDeath !== 'number') {
+			arg[i].yearOfDeath = 2024;
+		}
+		
+        let age = arg[i].yearOfDeath - arg[i].yearOfBirth;
 
+        if (age > oldest) {
+            oldest = age;
+			name = arg[i];
+        }
+    }
+	
+	console.log(name);
+    return name;
 };
 
 // Do not edit below this line
