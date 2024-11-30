8146002dfd86bbfdb52eb3bb9dee3eedb8d3f053
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-30 18:36:01 +0300

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
 
+    if (arg == 0) return 0;
+    if (arg < 0) return 'OOPS';
+
+	let list = [1, 1];
+    
+    for (let i = 1; i <= arg-2; i++) {
+      let newNumber = list.at(-1) + list.at(-2);
+      list.push(newNumber);
+    }
+  
+	return list.at(-1);
 };
 
 // Do not edit below this line
